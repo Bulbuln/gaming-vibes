c9ebf217070167ce7967959f87aa468fcf73cf2b
Bulbuln <60305187+Bulbuln@users.noreply.github.com>
2021-05-04 04:58:31 +0300

    login & logout

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,12 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { useContext } from 'react'
+import AuthContext from '../stores/authContext'
 
 export default function Navbar() {
+
+  const {user, login, logOut} = useContext(AuthContext)
+  
   return (
     <div className="container">
       <nav>
@@ -10,6 +15,9 @@ export default function Navbar() {
         <ul>
           <li><Link href="/"><a>Home</a></Link></li>
           <li><Link href="/guides"><a>Guides</a></Link></li>
+          <li onClick={login} className="btn">Login/Signup</li> 
+          <li onClick={logOut} className="btn">Logout</li> 
+
         </ul>
       </nav>
       <div className="banner">
